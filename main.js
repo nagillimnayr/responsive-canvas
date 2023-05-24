99576c360073a2118fa1c57d38f7881617faa66c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-23 22:27:40 -0700

    feat(main): add canvas to DOM

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function main() {
-
+    // Create canvas
+    const canvas = document.createElement('canvas');
+    canvas.id = 'canvas';
+    // attach the canvas to the DOM
+    document.body.appendChild(canvas);
+    
 }
 window.addEventListener('load', main);
